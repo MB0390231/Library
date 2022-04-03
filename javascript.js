932d9b5bb9a5a4e5e9020f071e9a646350fb2c56
MB0390231 <Michaelbroyles68@gmail.com>
2022-04-03 18:05:42 -0500

    added prototype method

--- javascript.js
+++ javascript.js
@@ -12,16 +12,30 @@ function book (title,author,pages) {
     this.pages = Number(pages),
     this.read = false
 }
+book.prototype.addBooktoLibrary = function () {
+    library.push(this)
+}
+book.prototype.createDiv = function () {
+    string = `<h1>${this.title}</h1><p>${this.author}</p><p>${this.pages}</p>`;
+    newBook = document.createElement('div');
+    newBook.classList = 'card'
+    newBook.innerHTML = string;
+    document.querySelector('.library').appendChild(newBook)
+}
 
 document.querySelector('.button').addEventListener('click',function () {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector('#pages').value
-
-    string = `<div class="card"><h1>${title}</h1><p>${author}</p><p>${pages}</p></div>`;
-    newBook = document.createElement('div');
-    newBook.classList = 'card'
-    newBook.innerHTML = string;
-    document.querySelector('.library').appendChild(newBook)
+    let newAddition = new book(title,author,pages)
+    newAddition.addBooktoLibrary()
+    newAddition.createDiv()
     document.querySelector(".form").reset()
+
+    // string = `<h1>${title}</h1><p>${author}</p><p>${pages}</p>`;
+    // newBook = document.createElement('div');
+    // newBook.classList = 'card'
+    // newBook.innerHTML = string;
+    // document.querySelector('.library').appendChild(newBook)
+    // document.querySelector(".form").reset()
 })
